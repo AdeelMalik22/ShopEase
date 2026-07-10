b790cd300796598550da853216c59269588588ee
Subhan Adeel <subhan.adeel@shopease.com>
2026-07-10 11:50:05 +0500

    updated celery config

--- notifications/celery_client.py
+++ notifications/celery_client.py
@@ -1,24 +1,11 @@
+import os
+
 from celery import Celery
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
-celery = Celery(
-    "ecommerce",
-    broker="redis://localhost:6379/0",
-    backend="redis://localhost:6379/1",
-)
+celery = Celery("ecommerce")
 
-celery.conf.update(
-    task_serializer="json",
-    result_serializer="json",
-    accept_content=["json"],
+celery.config_from_object("django.conf:settings", namespace="CELERY")
 
-    task_track_started=True,
-
-    task_acks_late=True,
-    task_reject_on_worker_lost=True,
-
-    worker_prefetch_multiplier=1,
-
-    timezone="UTC",
-    enable_utc=True,
-)
+celery.autodiscover_tasks()
